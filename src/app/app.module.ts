b28ef41f14ecbc872e6c3d06d292a313897fa148
AntonKalaturnyi <kalaturny@gmail.com>
2020-03-09 20:20:38 +0200

    AppRoutingModule created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     AppRoutingModule
   ],
   providers: [],
