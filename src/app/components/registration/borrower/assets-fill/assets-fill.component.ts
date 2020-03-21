3a44d1772a18c4db9f78393f7f4d4e658e4b8b5a
AntonKalaturnyi <kalaturny@gmail.com>
2020-03-21 22:23:27 +0200

    AssetsFillComponent added

--- src/app/components/registration/borrower/assets-fill/assets-fill.component.ts
+++ src/app/components/registration/borrower/assets-fill/assets-fill.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PermissionService } from 'src/app/services/permission.service';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { AlertService } from 'src/app/services/alert.service';
 import { UserService } from 'src/app/services/user.service';
 import { Router } from '@angular/router';
@@ -14,15 +14,44 @@ export class AssetsFillComponent implements OnInit {
 
   constructor(public permissionService: PermissionService, private formBuilder: FormBuilder, private alertService: AlertService,
               private userService: UserService, private router: Router) {
+                this.dataForm = this.formBuilder.group({
+                  flat: ['', [Validators.required]],
+                  hasHouse: ['', [Validators.required]],
+                  hasCar: ['', [Validators.required]],
+                  carAge: [''],
+                  wasAbroad: ['', [Validators.required]],
+                });
   }
 
   dataForm;
+  flats: string[] = ['Немає', 'Так, одна', 'Так, більше одної'];
+  yesNoOptions: string[] = ['Так', 'Ні'];
+  carAges: string[] = ['0-2 роки', '2-5 років', 'більше 5 років'];
+
+
 
   ngOnInit(): void {
   }
 
   submit(form) {
-    
-  }
+    if (form.hasHouse === 'Так') {
+      form.hasHouse = true;
+    } else { form.hasHouse = false; }
 
+    if (form.hasCar === 'Так') {
+      form.hasCar = true;
+    } else { form.hasCar = false; }
+
+    if (form.wasAbroad === 'Так') {
+      form.wasAbroad = true;
+    } else { form.wasAbroad = false; }
+
+    this.userService.saveBorrowerAssets(form).subscribe(data => {
+      this.alertService.successMessage('Реєстрацію завершено!', 'Супер');
+      this.router.navigateByUrl('borrower-cabinet');
+    }, error => {
+      console.log(error);
+      this.alertService.errorMessage(error.error.message, 'Invalid input');
+    });
+  }
 }
