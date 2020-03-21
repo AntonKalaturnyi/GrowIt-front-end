7426587203eb7c1feeafdd11a3b48f019604e83b
Anton Kalaturnyi <31220393+AntonKalaturnyi@users.noreply.github.com>
2020-03-21 22:24:18 +0200

    AssetsFillComponent added
    * login page works
    
    * registration of Borrower and Investor works
    
    * Login refactored with Creds model
    
    * template form without back-end calls works
    
    * Investor registration: Personal Ingo page with SMS works
    
    * Investor registration: passport & itn page works
    
    * Borrower is created from Creds
    
    * Borrower registration(1): Personal Info page with SMS works
    
    * PermissionService: Role url security added
    
    * borrower reg page(2) - passport/itn fill works
    
    * borrower reg page(3) - saveLivingAddress works
    
    * EducationFill rough created
    
    * other changes missed from previous commit
    
    * LoanCalculatorComponent added
    
    * description added to LoanCalculatorComponent
    
    * EducationFillComponent added
    
    * BorrowerPassportFillComponent validation fixed
    
    * AssetsFillComponent added

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -71,6 +71,12 @@ export class UserService {
     .pipe(catchError(this.errorHandler));
   }
 
+  saveBorrowerAssets(dto) {
+    const headers = this.getTokenHeader();
+    return this.http.post('http://localhost:8080/GrowIt/borrower/set-assets', dto, { headers })
+    .pipe(catchError(this.errorHandler));
+  }
+
   errorHandler(error: HttpErrorResponse) {
     return throwError(error || 'Server error');
   }
