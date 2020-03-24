9c264f6b7f15c6ee28885f8f4155809ce999dc9c
AntonKalaturnyi <kalaturny@gmail.com>
2020-03-24 09:50:44 +0200

    saved input field load for AddressFillComponent added

---
+++ src/app/model/BorrowerAddressData.ts
@@ -0,0 +1,12 @@
+export class BorrowerAddressData {
+
+    sameAddressInPassport: boolean;
+    region: string;
+    district: string;
+    postalCode: string;
+    settlement: string;
+    street: string;
+    number: string;
+    corpsNo: string;
+    door: string;
+}
