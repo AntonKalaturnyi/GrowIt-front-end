4cc63351c80413c0d93e8f959df9961f82c035c4
Anton Kalaturnyi <31220393+AntonKalaturnyi@users.noreply.github.com>
2020-03-29 23:04:05 +0300

    DashboardComponent mock data inserted
    * login page works
    
    * registration of Borrower and Investor works
    
    * Login refactored with Creds model
    
    * template form without back-end calls works
    
    * Investor registration: Personal Ingo page with SMS works
    
    * Investor registration: passport & itn page works
    
    * Borrower is created from Creds
    
    * Borrower registration(1): Personal Info page with SMS works
    
    * PermissionService: Role url security added
    
    * borrower reg page(2) - passport/itn fill works
    
    * borrower reg page(3) - saveLivingAddress works
    
    * EducationFill rough created
    
    * other changes missed from previous commit
    
    * LoanCalculatorComponent added
    
    * description added to LoanCalculatorComponent
    
    * EducationFillComponent added
    
    * BorrowerPassportFillComponent validation fixed
    
    * AssetsFillComponent added
    
    * EmploymentFillComponent improved
    
    * saved input field load for BorrowerRegComponent added
    
    * saved input field load for BorrowerPassportFillComponent added
    
    * saved input field load for AddressFillComponent added
    
    * saved input load for EmploymentFillComponent added
    
    * saved input load for EducationFillComponent added
    
    * DashboardComponent mock data

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { PermissionService } from 'src/app/services/permission.service';
+import { FormBuilder } from '@angular/forms';
+import { AlertService } from 'src/app/services/alert.service';
+import { UserService } from 'src/app/services/user.service';
+import { PrevDataService } from 'src/app/services/prev-data.service';
+import { Router } from '@angular/router';
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
 
 @Component({
   selector: 'app-dashboard',
@@ -7,7 +33,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(public permissionService: PermissionService, private formBuilder: FormBuilder, private alertService: AlertService,
+    private userService: UserService, private dataService: PrevDataService, private router: Router) { }
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
 
   ngOnInit(): void {
   }
