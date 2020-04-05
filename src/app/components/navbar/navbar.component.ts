d0a12974a85639c1854bdeabfc9e7f7967355495
Anton Kalaturnyi <31220393+AntonKalaturnyi@users.noreply.github.com>
2020-04-05 21:19:46 +0300

    basic navbar and footer added
    * login page works
    
    * registration of Borrower and Investor works
    
    * Login refactored with Creds model
    
    * template form without back-end calls works
    
    * Investor registration: Personal Ingo page with SMS works
    
    * Investor registration: passport & itn page works
    
    * Borrower is created from Creds
    
    * Borrower registration(1): Personal Info page with SMS works
    
    * PermissionService: Role url security added
    
    * borrower reg page(2) - passport/itn fill works
    
    * borrower reg page(3) - saveLivingAddress works
    
    * EducationFill rough created
    
    * other changes missed from previous commit
    
    * LoanCalculatorComponent added
    
    * description added to LoanCalculatorComponent
    
    * EducationFillComponent added
    
    * BorrowerPassportFillComponent validation fixed
    
    * AssetsFillComponent added
    
    * EmploymentFillComponent improved
    
    * saved input field load for BorrowerRegComponent added
    
    * saved input field load for BorrowerPassportFillComponent added
    
    * saved input field load for AddressFillComponent added
    
    * saved input load for EmploymentFillComponent added
    
    * saved input load for EducationFillComponent added
    
    * DashboardComponent mock data
    
    * DashboardComponent table improved: front fields reflected
    
    * DashboardCompont: accordion table added
    
    * DashboardCompont: expandable loan details  ;dynamic income count per each loan
    
    * DashboardCompont: InvestService added; sendInvestments() works
    
    * auth and JWT token expiration refactored; double add money on dashboard fixed; [disabled] added for 'Register' button
    
    * basic navbar and footer added

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,17 +1,21 @@
-import { Component, OnInit, ViewEncapsulation, AfterViewInit, Output, EventEmitter, HostListener } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
 
-  ngOnInit(): void {
-  }
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
 
+  constructor(private breakpointObserver: BreakpointObserver) {}
 
-
-  }
+}
