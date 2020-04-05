4ba0180da2f1bcba55e21a54c17773da5829fc47
Anton Kalaturnyi <31220393+AntonKalaturnyi@users.noreply.github.com>
2020-04-05 22:28:54 +0300

    basic navbar and footer added repeat commit
    * login page works
    
    * registration of Borrower and Investor works
    
    * Login refactored with Creds model
    
    * template form without back-end calls works
    
    * Investor registration: Personal Ingo page with SMS works
    
    * Investor registration: passport & itn page works
    
    * Borrower is created from Creds
    
    * Borrower registration(1): Personal Info page with SMS works
    
    * PermissionService: Role url security added
    
    * borrower reg page(2) - passport/itn fill works
    
    * borrower reg page(3) - saveLivingAddress works
    
    * EducationFill rough created
    
    * other changes missed from previous commit
    
    * LoanCalculatorComponent added
    
    * description added to LoanCalculatorComponent
    
    * EducationFillComponent added
    
    * BorrowerPassportFillComponent validation fixed
    
    * AssetsFillComponent added
    
    * EmploymentFillComponent improved
    
    * saved input field load for BorrowerRegComponent added
    
    * saved input field load for BorrowerPassportFillComponent added
    
    * saved input field load for AddressFillComponent added
    
    * saved input load for EmploymentFillComponent added
    
    * saved input load for EducationFillComponent added
    
    * DashboardComponent mock data
    
    * DashboardComponent table improved: front fields reflected
    
    * DashboardCompont: accordion table added
    
    * DashboardCompont: expandable loan details  ;dynamic income count per each loan
    
    * DashboardCompont: InvestService added; sendInvestments() works
    
    * auth and JWT token expiration refactored; double add money on dashboard fixed; [disabled] added for 'Register' button
    
    * basic navbar and footer added
    
    * basic navbar and footer added repeat commit

--- src/app/services/permission.service.ts
+++ src/app/services/permission.service.ts
@@ -4,13 +4,14 @@ import { Observable, throwError  } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { UserService } from './user.service';
 import { Creds } from '../model/Creds';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PermissionService {
 
-  constructor(private http: HttpClient, private userService: UserService) { }
+  constructor(private http: HttpClient, private userService: UserService, private router: Router) { }
 
   registeredUserPermission(): boolean {
     if (localStorage.getItem('REGISTERED_USER')) {
@@ -21,9 +22,13 @@ export class PermissionService {
   }
 
   public getTokenHeader(): HttpHeaders {
+    if (((Number.parseInt(localStorage.getItem('tokenReceivedAt')) + 3600000) - new Date().getTime()) < 0 ) {  // fix: when remove +3600000 it disturbs the request and red in console
+      localStorage.clear();
+      this.router.navigateByUrl('/login');
+    }
     let headers = new HttpHeaders();
     const token = localStorage.getItem('token');
-    if ( ((Number.parseInt(localStorage.getItem('tokenReceivedAt')) + 3600000) - new Date().getTime()) <= 600000) {
+    if ( ((Number.parseInt(localStorage.getItem('tokenReceivedAt')) + 3600000) - new Date().getTime()) <= 1800000 ) {
         const creds = new Creds();
         creds.username = localStorage.getItem('email');
         creds.password = localStorage.getItem('password');
