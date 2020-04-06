abd513dbaf3c8d1bd042b12d00148a56baff3d88
Anton Kalaturnyi <31220393+AntonKalaturnyi@users.noreply.github.com>
2020-04-06 10:53:19 +0300

    footer component added; navbar button logic added
    * login page works
    
    * registration of Borrower and Investor works
    
    * Login refactored with Creds model
    
    * template form without back-end calls works
    
    * Investor registration: Personal Ingo page with SMS works
    
    * Investor registration: passport & itn page works
    
    * Borrower is created from Creds
    
    * Borrower registration(1): Personal Info page with SMS works
    
    * PermissionService: Role url security added
    
    * borrower reg page(2) - passport/itn fill works
    
    * borrower reg page(3) - saveLivingAddress works
    
    * EducationFill rough created
    
    * other changes missed from previous commit
    
    * LoanCalculatorComponent added
    
    * description added to LoanCalculatorComponent
    
    * EducationFillComponent added
    
    * BorrowerPassportFillComponent validation fixed
    
    * AssetsFillComponent added
    
    * EmploymentFillComponent improved
    
    * saved input field load for BorrowerRegComponent added
    
    * saved input field load for BorrowerPassportFillComponent added
    
    * saved input field load for AddressFillComponent added
    
    * saved input load for EmploymentFillComponent added
    
    * saved input load for EducationFillComponent added
    
    * DashboardComponent mock data
    
    * DashboardComponent table improved: front fields reflected
    
    * DashboardCompont: accordion table added
    
    * DashboardCompont: expandable loan details  ;dynamic income count per each loan
    
    * DashboardCompont: InvestService added; sendInvestments() works
    
    * auth and JWT token expiration refactored; double add money on dashboard fixed; [disabled] added for 'Register' button
    
    * basic navbar and footer added
    
    * basic navbar and footer added repeat commit
    
    * footer component added; navbar button logic added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -51,6 +51,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
@@ -71,6 +72,7 @@ import { MatListModule } from '@angular/material/list';
     AssetsFillComponent,
     DashboardComponent,
     NavbarComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserAnimationsModule,
