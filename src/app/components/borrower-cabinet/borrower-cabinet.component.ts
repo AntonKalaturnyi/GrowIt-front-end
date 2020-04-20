07d4676ed557ab5d006af990c693dd256ca04557
AntonKalaturnyi <kalaturny@gmail.com>
2020-04-20 22:02:06 +0300

    basic fields transfer for BorrowerCabinet works

--- src/app/components/borrower-cabinet/borrower-cabinet.component.ts
+++ src/app/components/borrower-cabinet/borrower-cabinet.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PermissionService } from 'src/app/services/permission.service';
+import { BorrowerCabinetService } from 'src/app/services/borrower-cabinet.service';
 
 @Component({
   selector: 'app-borrower-cabinet',
@@ -8,9 +9,22 @@ import { PermissionService } from 'src/app/services/permission.service';
 })
 export class BorrowerCabinetComponent implements OnInit {
 
-  constructor(public permissionService: PermissionService) { }
+  constructor(public permissionService: PermissionService, private cabinetService: BorrowerCabinetService) { }
+
+  availableBalance: number;
+  dailyRate: number;
+  rank: string;
+  score: string;
+  status: string;
 
   ngOnInit(): void {
+    this.cabinetService.getData().subscribe(data => {
+      this.availableBalance = data.availableBalance;
+      this.dailyRate = data.dailyRate;
+      this.rank = data.rank;
+      this.score = data.score;
+      this.status = data.status;
+    });
   }
 
 }
