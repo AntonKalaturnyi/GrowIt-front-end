69021a85111d57a5b48dea67d83d439c1d905c4a
AntonKalaturnyi <kalaturny@gmail.com>
2020-05-10 23:40:26 +0300

    InvestorCabinetService gets availableBalance/investedFunds gor cabinet

---
+++ src/app/services/investor-cabinet.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { PermissionService } from './permission.service';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InvestorCabinetService {
+
+  constructor(private http: HttpClient, private permissionService: PermissionService) { }
+
+  getData(): Observable<any> {
+    const headers = this.permissionService.getTokenHeader();
+    return this.http.get<any>('http://localhost:8080/GrowIt/investor-account/get-data', { headers })
+    .pipe(catchError(this.permissionService.errorHandler));
+  }
+}
